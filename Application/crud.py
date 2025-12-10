4ea04af102868768ef8af04bc0c2bf496204b390
eclctc <111204234+eclctc@users.noreply.github.com>
2025-12-10 14:44:29 -0500

    Add logic to retrieve a single user from the db

--- Application/crud.py
+++ Application/crud.py
@@ -1,3 +1,12 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import update
 import models, schemas
+
+
+# Fetches users from the database with optional pagination, skipping skip records and limiting results to limit (default 100). It returns the retrieved users as a list.
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+# Fetches a single user from the database by their ID. It returns the retrieved user as a response.
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
