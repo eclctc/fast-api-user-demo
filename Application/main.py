edcc73edb6e41f1e38a2acfb04f2c784d74d498d
eclctc <111204234+eclctc@users.noreply.github.com>
2025-12-10 14:12:51 -0500

    Adding database session logic

--- Application/main.py
+++ Application/main.py
@@ -2,13 +2,11 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
+from database import SessionLocal, engine
 from typing import List
 import crud
 import models
 import schemas
-
-
-
 
 app = FastAPI()
 
@@ -17,3 +15,11 @@ app = FastAPI()
 async def docs_redirect():
     response = RedirectResponse(url='/docs')
     return response
+
+# Manages the database session lifecycle. It creates a new SQLAlchemy session session using SessionLocal() method and yields this session for use.
+def get_db():
+  db = SessionLocal()
+  try:
+      yield db
+  finally:
+      db.close()
