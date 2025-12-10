f68968f1816649c4904189989c5fbd572edf299c
eclctc <111204234+eclctc@users.noreply.github.com>
2025-12-10 15:12:21 -0500

    Add database call to get user by email

--- Application/crud.py
+++ Application/crud.py
@@ -10,3 +10,7 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 # Fetches a single user from the database by their ID. It returns the retrieved user as a response.
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
+
+# Fetches a single user from the database by their email. It returns the retrieved user as a response.
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
