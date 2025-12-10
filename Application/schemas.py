3716051915610284b95134477a841d6b07990e8f
eclctc <111204234+eclctc@users.noreply.github.com>
2025-12-10 13:58:03 -0500

    Create user base class from BaseModel

--- Application/schemas.py
+++ Application/schemas.py
@@ -1,2 +1,11 @@
 from pydantic import BaseModel
 from typing import List
+
+class UserBase(BaseModel):
+    email: str
+    username: str
+    first_name: str
+    last_name: str
+    gender: str
+    country: str
+    isActive: bool
